d0043061b1f170c025f5388386d49823784caca1
Robotics Student <student@TENB885849C6BB9.shefuniad.shef.ac.uk>
2022-05-17 17:13:28 +0100

    change

--- task3/server.py
+++ task3/server.py
@@ -123,7 +123,7 @@ class SearchActionServer(object):
             self.vel_controller.publish()
             # check if there has been a request to cancel the action mid-way through:
             if self.actionserver.is_preempt_requested():
-                rospy.loginfo("Cancelling the camera sweep.")
+                rospy.loginfo("Cancelling the camera sweep...")
                 self.actionserver.set_preempted()
                 # stop the robot:
                 self.vel_controller.stop()
